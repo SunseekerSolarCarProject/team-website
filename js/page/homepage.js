900d57dca762a8348a0b04d7e59593ce9f953e2b
Alec Carpenter <alec.g.carpenter@wmich.edu>
2015-03-24 12:35:15 -0400

    Changed konami video from m4v to mov

--- js/page/homepage.js
+++ js/page/homepage.js
@@ -5,50 +5,50 @@ require(['jquery', 'slideshow', 'konami'], function($) {
     konamiGoing = false;
 
     $(window).konami({
-    	cheat: function() {
-    		if(konamiGoing)
-    			return;
+        cheat: function() {
+            if(konamiGoing)
+                return;
 
-    		konamiGoing = true;
+            konamiGoing = true;
 
-    		var c, v, s, bg, ht;
+            var c, v, s, bg, ht;
 
-    		bigSlide.stop();
-    		window.scrollTo(0, 0);
+            bigSlide.stop();
+            window.scrollTo(0, 0);
 
-    		c  = bigSlide.current();
-    		v  = $('<video>');
-    		s  = $('<source>');
-    		bg = c.css('background-image');
-    		ht = c.css('height');
+            c  = bigSlide.current();
+            v  = $('<video>');
+            s  = $('<source>');
+            bg = c.css('background-image');
+            ht = c.css('height');
 
-    		c.css('height', '720px');
-    		c.css('background-color', '#000');
-    		c.css('background-image', 'none');
-    		c.css('text-indent', '0px');
-    		c.text('');
+            c.css('height', '720px');
+            c.css('background-color', '#000');
+            c.css('background-image', 'none');
+            c.css('text-indent', '0px');
+            c.text('');
 
-    		v.attr('width', '100%');
-    		v.attr('height', '720');
-    		v.attr('controls', 'true');
-    		v.attr('autoplay', 'true');
+            v.attr('width', '100%');
+            v.attr('height', '720');
+            v.attr('controls', 'true');
+            v.attr('autoplay', 'true');
 
-    		v.bind('ended', function() {
-    			v.remove();
+            v.bind('ended', function() {
+                v.remove();
 
-    			c.css('background-color', 'transparent');
-    			c.css('background-image', bg);
-    			c.css('height', ht);
+                c.css('background-color', 'transparent');
+                c.css('background-image', bg);
+                c.css('height', ht);
 
-    			konamiGoing = false;
-    			bigSlide.start();
-    		});
+                konamiGoing = false;
+                bigSlide.start();
+            });
 
-    		s.attr('src', 'videos/DriverStyle.m4v');
-    		s.attr('type', 'video/mp4');
+            s.attr('src', 'videos/DriverStyle.mov');
+            s.attr('type', 'video/quicktime');
 
-    		v.append(s);
-    		c.append(v);
-    	}
+            v.append(s);
+            c.append(v);
+        }
     });
 });
