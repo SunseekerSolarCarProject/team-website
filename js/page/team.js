6e5381e91f659ecd9935dfaa5c95b94879bae85d
gunnar94 <gunnar94@me.com>
2015-10-15 11:59:28 -0400

    Updated abe poot memorial blog with parking info

--- js/page/team.js
+++ js/page/team.js
@@ -1,12 +1,35 @@
 require(['jquery', 'konami'], function($) {
-    var konamiPeeps = [];
+    var konamiPeeps = [],
+        enabled     = false;
 
     konamiPeeps = $('img[data-konami-picture]');
 
     $(window).konami({
         cheat: function () {
+            if (enabled)
+                return;
+
+            enabled = true;
+
             konamiPeeps.each(function () {
-                $(this).attr('src', $(this).data('konami-picture'));
+                var $this = $(this),
+                    src   = $this.attr('src');
+
+                $this.fadeOut(function () {
+                    $this.attr('src', $this.data('konami-picture'));
+
+                    $this.fadeIn(function () {
+                        setTimeout(function () {
+                            $this.fadeOut(function () {
+                                $this.attr('src', src);
+
+                                $this.fadeIn(function () {
+                                    enabled = false;
+                                });
+                            });
+                        }, 10000);
+                    });
+                });
             });
         }
     });
