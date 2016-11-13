d2dfd21eaf27e7d59f279ef43a16260f37f2a290
alecgunnar <alecgunnar@gmail.com>
2016-11-13 17:07:38 -0500

    Added donation wizard to sponsor page

---
+++ _js/donate.js
@@ -0,0 +1,49 @@
+var $ = require('jquery');
+
+var $activeDonationAmount;
+var $otherAmount;
+var $continue;
+var amount;
+
+$(window).ready(function() {
+    $otherAmount = $('.donationAmounts__amount--other input');
+    $otherAmount.keyup(function() {
+        var $this = $(this);
+        amount = $this.val().replace(/\D/g, '');
+
+        $this.val(amount);
+
+        if (amount > 0) {
+            toggleActiveAmount(null);
+        }
+    });
+
+    $continue = $('.chooseDonation__continue');
+    $continue.on('click', function() {
+        if (!amount) {
+            alert('You must choose an amount greater than zero!');
+            return;
+        }
+
+        window.location = $continue.data('url') + amount;
+    });
+
+    $('.donationAmounts__amount__clickable').each(function() {
+        $(this).on('click', function() {
+            toggleActiveAmount($(this));
+            amount = $activeDonationAmount.data('amount');
+        });
+    });
+});
+
+function toggleActiveAmount($new) {
+    if ($activeDonationAmount) {
+        $activeDonationAmount.toggleClass('active');
+    }
+
+    if ($new) {
+        $new.toggleClass('active');
+    }
+
+    $activeDonationAmount = $new;
+}
